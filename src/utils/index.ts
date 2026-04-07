81f542097ec6d30324b3399d3ac2c3c01e5b8758
Glinsky Ivan <glinsky_ivan@mail.ru>
2026-04-07 19:54:16 +0500

    добавил утилиту для получения order параметров

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,8 @@ export {getSliceOrientation, getSliceOrientationFromSeriesDescription} from './g
 export {getProrocolName} from './getProrocolName';
 export {buildFindAllParams} from './buildFindAllParams';
 export {buildWhere} from './buildWhere';
+export {buildOrder} from './buildOrder';
 
 export type {FindAllServiceParams} from './buildFindAllParams';
 export type {BuildWhereParams} from './buildWhere';
+export type {BuildOrderParams} from './buildOrder';
