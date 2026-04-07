c099194c66793f63d25d32e996d5ad53e0576863
Glinsky Ivan <glinsky_ivan@mail.ru>
2026-04-07 19:44:26 +0500

    добавил утилиту для получения where параметров

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,7 @@ export {dicomDateToISO} from './dicomDateToISO';
 export {getSliceOrientation, getSliceOrientationFromSeriesDescription} from './getOrientation';
 export {getProrocolName} from './getProrocolName';
 export {buildFindAllParams} from './buildFindAllParams';
+export {buildWhere} from './buildWhere';
 
 export type {FindAllServiceParams} from './buildFindAllParams';
+export type {BuildWhereParams} from './buildWhere';
